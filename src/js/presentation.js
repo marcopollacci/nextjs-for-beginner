b122b74098b3096598458dc03bf79d463dddf973
Marco Pollacci <marco.pollacci@antreem.it>
2024-03-08 13:05:39 +0100

    feat(main): :sparkles: adding new fragment conditional

--- src/js/presentation.js
+++ src/js/presentation.js
@@ -1,1 +1,29 @@
-CSS.paintWorklet.addModule('./js/sketchy-arrow.js');
+CSS.paintWorklet.addModule("./js/sketchy-arrow.js");
+
+export function listenGroupFragment(nameFragment) {
+  const observer = new MutationObserver((mutations) => {
+    mutations.forEach(() => {
+      const elements = document.querySelectorAll(
+        '[group-fragment="project-structure"]'
+      );
+      elements.forEach((el, index, nodeList) => {
+        const ariaHidden = el.getAttribute("aria-hidden");
+        if (index > 0) {
+          nodeList[index - 1].style.display =
+            ariaHidden === "false" ? "none" : "inherit";
+        }
+      });
+    });
+  });
+
+  document
+    .querySelectorAll(`[group-fragment="${nameFragment}"]`)
+    .forEach((el) => {
+      observer.observe(el, {
+        attributes: true,
+        attributeFilter: ["aria-hidden"],
+      });
+    });
+}
+
+listenGroupFragment("project-structure");
