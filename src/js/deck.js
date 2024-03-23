86b264784d02bbd9bfa7725fc510d65a1d542d2b
Marco Pollacci <pollacci@gmail.com>
2024-03-23 12:53:47 +0100

    feat(testing): :sparkles: adding testing deck

--- src/js/deck.js
+++ src/js/deck.js
@@ -1,12 +1,13 @@
 const styleHeader = document.createElement('style');
-styleHeader.innerHTML = `header { display: flex;
+styleHeader.innerHTML = `header { 
+  display: flex;
   align-items: center;
   gap: 0.4em;
   :first-child {
     margin-inline-end: -0.4em;
   }
   :nth-child(3) {
-    margin-inline-end: -0.3em;
+    margin-inline-end: -0.35em;
   }
  }`;
 
